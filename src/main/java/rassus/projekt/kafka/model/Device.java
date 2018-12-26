9478fab9ecf2a2c72b61b1a5d4fd887ffcf6f8a1
dariansaric <darian.saric@fer.hr>
2018-12-26 12:44:39 +0100

    Napravljen početak simulacije uređaja

---
+++ src/main/java/rassus/projekt/kafka/model/Device.java
@@ -0,0 +1,9 @@
+package rassus.projekt.kafka.model;
+
+public class Device {
+    //TODO: dopuniti
+    private static int INSTANCE_COUNT = 0;
+    private int id;
+    private String name;
+    private MetricGenerator generator; //TODO: privatna implementacija ili generalna
+}
---
+++ src/main/java/rassus/projekt/kafka/model/MetricGenerator.java
@@ -0,0 +1,56 @@
+package rassus.projekt.kafka.model;
+
+/**
+ * Ovo sučelje definira tvornicu mjerenja. Ideja je da implementacija sučelja
+ * ima pristup nekoj datoteci gdje pišu parametri uređaja, zatim na temelju istih
+ * generira neki rezultat. Ovo je podložno provjerama naravno :)
+ */
+public interface MetricGenerator {
+
+    /**
+     * Vraća CPU usage
+     *
+     * @param deviceId id nekog uređaja
+     *
+     * @return cpu usage
+     */
+    int generateCPUUsage(int deviceId);
+
+    /**
+     * Vraća Memory usage
+     *
+     * @param deviceId id uređaja
+     *
+     * @return memory usage
+     */
+    int generateMemoryUsage(int deviceId);
+
+    /**
+     * Vraća broj aktivnih veza.
+     *
+     * @param deviceId id uređaja
+     *
+     * @return broj veza
+     */
+    int generateNoConnections(int deviceId);
+
+    /**
+     * vraća uređeni par (poslanih,primljenih) TCP paketa.
+     *
+     * @param deviceId id uređaja
+     *
+     * @return TCP poslano, primljeno
+     */
+    int[] generateTCPTraffic(int deviceId);
+
+    /**
+     * vraća uređeni par (poslanih,primljenih) UDP paketa.
+     *
+     * @param deviceId id uređaja
+     *
+     * @return UDP poslano, primljeno
+     */
+    int[] generateUDPTraffic(int deviceId);
+
+    //TODO: dopuniti
+}
