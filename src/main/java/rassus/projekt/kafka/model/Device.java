a8de2481e612d1b5d21841f4853fa57ea40b93d5
dariansaric <darian.saric@fer.hr>
2018-12-26 17:20:20 +0100

    Dopunjena neophodna dokumentacija

--- src/main/java/rassus/projekt/kafka/model/Device.java
+++ src/main/java/rassus/projekt/kafka/model/Device.java
@@ -3,10 +3,22 @@ package rassus.projekt.kafka.model;
 import rassus.projekt.kafka.util.DefaultMetricGenerator;
 import rassus.projekt.kafka.util.MetricGenerator;
 
+/**
+ * Ovaj razred simulira uređaj u mreži. Pri pokretanju iz naredbenog retka uzima id iz liste argumenata
+ * i čita vlastitu konfiguraciju. Periodično generira mjerenja i šalje na Kafka klaster.
+ */
 public class Device {
     //TODO: dopuniti
     private static int INSTANCE_COUNT = 0;
-    private int id;
     //    private String name;
     private final MetricGenerator generator = new DefaultMetricGenerator(); //TODO: privatna implementacija ili generalna
+    private int id;
+
+    public Device(int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException("Neispravan id uređaja: " + id);
+        }
+        this.id = id;
+        INSTANCE_COUNT++;
+    }
 }
--- src/main/java/rassus/projekt/kafka/util/DefaultMetricGenerator.java
+++ src/main/java/rassus/projekt/kafka/util/DefaultMetricGenerator.java
@@ -11,23 +11,48 @@ import static java.lang.StrictMath.toIntExact;
  * koristit će se normalna (Gaussova) razdioba i njena implementacija u {@link java.util.Random}.
  */
 public class DefaultMetricGenerator implements MetricGenerator {
+    /**
+     * Mapa koja sadrži konfiguracije svih postojećih uređaja
+     */
     private static final Map<Integer, Config> k = fillMap(); //TODO: statička konfiguracija ili periodično čitanje
+    /**
+     * {@link Random} objekt za nasumična mjerenja
+     */
     private static final Random RANDOM = new Random();
 
+    /**
+     * @return popunjena mapa konfiguracija svih uređaja
+     */
     private static Map<Integer, Config> fillMap() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Vraća {@link Config} objekt za primljeni id, ako postoji.
+     *
+     * @param deviceId id uređaja
+     *
+     * @return Config uređaja
+     */
+    private static Config getConfigForId(int deviceId) {
+        Config c = k.get(deviceId);
+        if (c == null) {
+            throw new IllegalArgumentException("Uređaj s id: " + deviceId + " ne postoji...");
+        }
+
+        return c;
+    }
+
     @Override
     public int generateCPUUsage(int deviceId) {
-        Config c = k.get(deviceId);
+        Config c = getConfigForId(deviceId);
         //TODO: vidi todu u razredu Config
         return toIntExact(round(RANDOM.nextGaussian() * c.getCpuSpike() + c.getAvgCpu()));
     }
 
     @Override
     public int generateMemoryUsage(int deviceId) {
-        Config c = k.get(deviceId);
+        Config c = getConfigForId(deviceId);
         return toIntExact(round(RANDOM.nextGaussian() * c.getRamSpike() + c.getAvgRam()));
     }
 
